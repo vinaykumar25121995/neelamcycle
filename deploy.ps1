71939b72049e70e264044170b94979979782a32a
vinaykumar25121995 <vinaykumar21y95@gmail.com>
2026-06-13 12:19:02 +0530

    Delete deploy.ps1

--- deploy.ps1
+++
@@ -1,8 +0,0 @@
-# Initialize Git, Commit, and Push
-git init
-git add .
-git commit -m "Initial commit for Neelam Cycle"
-git branch -M main
-git remote add origin https://github.com/vinaykumar25121995/neelamcycle.git
-# We'll use --force just in case the repository already exists and has a README
-git push -u origin main --force
